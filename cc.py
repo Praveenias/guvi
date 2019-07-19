70bf8bf1cb13ba41ff81d44e9fe5b51fef3b845a
Praveenias <43996168+Praveenias@users.noreply.github.com>
2019-07-19 14:11:32 +0530

    Create cc.py

---
+++ cc.py
@@ -0,0 +1,8 @@
+ch = input("")
+if((ch>='a' and ch<= 'z') or (ch>='A' and ch<='Z')):
+  if(ch=='a' or ch=='A' or ch=='E' or ch=='e' or ch=='i' or ch=='O' or ch=='o' or ch=='I' or ch=='U' or ch=='u'):
+    print("Vowel")
+  else:
+    print("consonent")      
+else:
+  print("invalid")
