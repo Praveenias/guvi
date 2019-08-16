6c4fbd87b19ef59257c4566b8b27edf27b665132
Praveenias <43996168+Praveenias@users.noreply.github.com>
2019-08-16 17:44:19 +0530

    Create b28.c

---
+++ b28.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b[20],i;
+    scanf("%d",&a);
+    for(i=0;i<a;i++)
+    {
+        scanf("%d",&b[i]);
+    }
+    for(i=0;i<a;i++)
+    {
+        printf("%d %d\n",b[i],i);
+    }    
+        
+    
+    return 0;
+}
